00b5d8ae4f3b790725cdc2b64531dd5bddbf7e67
Tony Wang <tkwang312@gmail.com>
2024-03-20 22:23:56 -0400

    using anything v5 model

--- sd-api/api.py
+++ sd-api/api.py
@@ -4,9 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
+from transformers import CLIPTextModel, CLIPTokenizer
 from io import BytesIO
 import base64
 import platform
+from PIL.Image import Image
 
 print(platform.platform())
 app = FastAPI()
@@ -19,17 +21,23 @@ app.add_middleware(
     allow_headers=["*"], # Allows all headers
 )
 
+# tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16)
+# text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16).to("cuda")
+
+
 device = "cuda"
-model_id = "CompVis/stable-diffusion-v1-4"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, variant="fp16", torch_dtype=torch.float16, token=token)
+model_id = "stablediffusionapi/anything-v5"
+# model_id = "CompVis/stable-diffusion-v1-4"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, use_safetensors=True, torch_dtype=torch.float16, token=token)
 pipe.to(device)
+
 
 @app.get("/")
 def generate(prompt: str): 
     with autocast(device): 
         image = pipe(prompt, guidance_scale=8.5).images[0]
 
-    image.save("testimage.png")
+    image.save(prompt + " testimage.png")
 
     buffer = BytesIO()
     image.save(buffer, format="PNG")
